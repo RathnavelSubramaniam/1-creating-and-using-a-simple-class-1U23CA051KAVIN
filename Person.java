77ab9ad9604f44b4b7b3e5679d9716f9e9d4a07d
1U23CA051KAVIN <139528325+1U23CA051KAVIN@users.noreply.github.com>
2024-01-20 04:50:47 +0000

    s1

--- Person.java
+++ Person.java
@@ -16,12 +16,13 @@ class Persona
  }
 class Person{
  public static void main (String arg[]){
-    Persona a1 = new Persona("jhon","Doe",30);
-    Persona a2 = new Persona("Alice","smith",25);
+    Persona a1 = new Persona("Jhon","Doe",30);
+    Persona a2 = new Persona("Alice","Smith",25);
     
     System.out.println("Person 1:"+a1.getfullname());
     System.out.println("Person 2:"+a2.getfullname());
     double average = ((a1.age + a2.age)/2.0);
     System.out.println("Average Age:"+average);
+    
  }
 } 
